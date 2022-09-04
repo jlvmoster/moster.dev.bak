9f3f08d0c6e8185637546ad56930fc524d622f18
Jalo Moster <jlvmoster@gmail.com>
2022-09-04 01:27:29 -0400

    CHANGELOG - Set style and code lint rules with Prettier and Eslint configs - Enable husky and lint-staged automated pre-commit setup to enforce eslint and prettier style formatting - Enable Node.js code assistance in WebStorm workspace - Specify eslint target directories in next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    dirs: ['pages', 'components', 'lib'],
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
