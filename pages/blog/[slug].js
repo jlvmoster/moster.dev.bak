fd4522f68b88aa8eb26f61656501250be7fd20cb
Jalo Moster <jlvmoster@gmail.com>
2022-09-08 16:02:32 -0400

    CHANGELOG - Version bump project to minor release v0.2.0 and start development - Update next version to v12.3.0 - Enhance Footer UI with a mobile-friendly corporate tag text - Minor changes to blog post field properties access strategy

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -27,30 +27,30 @@ const renderOptions = {
 
 const BlogPost = ({ router, post, readingTime, preview }) => (
   <>
-    <SEO title={post.title} description={post.excerpt} pathname={router.pathname} />
+    <SEO title={post.fields.title} description={post.fields.excerpt} pathname={router.pathname} />
     <Layout pathname={router.pathname} preview={preview}>
       <div className='mx-auto mt-8 mb-24 space-y-8 sm:space-y-12 sm:max-w-3xl'>
         <section className='flex px-4'>
           <div className='mx-auto pt-12 pb-6 px-8'>
-            <h1 className='font-mono text-5xl tracking-wide sm:text-7xl'>{post.title}</h1>
+            <h1 className='font-mono text-5xl tracking-wide sm:text-7xl'>{post.fields.title}</h1>
             <p className='font-medium text-xs sm:text-sm'>
-              {format(new Date(post.publishDate), 'LLLL d, yyyy')} &bull; {readingTime} min read
+              {format(new Date(post.fields.publishDate), 'EEEE, LLLL do yyyy')} &bull; {readingTime} min read
             </p>
             <div className='my-3 border-b-2 border-dotted sm:border-b-4' />
-            <h4 className='font-semibold text-sm sm:text-base'>{post.excerpt}</h4>
+            <h4 className='font-semibold text-sm sm:text-base'>{post.fields.excerpt}</h4>
           </div>
         </section>
         <div className='relative mx-auto aspect-w-16 aspect-h-9'>
           <Image
-            src={`https:${post.heroImage.fields.file.url}`}
+            src={`https:${post.fields.heroImage.fields.file.url}`}
             layout='fill'
-            alt={post.heroImage.fields.title}
+            alt={post.fields.heroImage.fields.title}
             priority
           />
         </div>
         <section className='px-4'>
           <article className='max-w-3xl mx-auto prose dark:prose-invert lg:prose-xl'>
-            {documentToReactComponents(post.content, renderOptions)}
+            {documentToReactComponents(post.fields.content, renderOptions)}
           </article>
         </section>
       </div>
@@ -63,13 +63,13 @@ export default withRouter(BlogPost);
 export const getStaticProps = async ({ params, preview = false }) => {
   const post = (await getBlogPostBySlug(params.slug, preview)) ?? null;
 
-  const wpm = 200; // average time a reader takes
+  const wpm = 200; // average time a reader takes (words per minute)
   const content = documentToPlainTextString(post.fields.content);
-  const words = content.trim().split(/\s+/).length;
+  const words = (content.match(/\w+/g) ?? []).length;
   const readingTime = Math.ceil(words / wpm);
 
   return {
-    props: { post: post.fields, readingTime, preview },
+    props: { post, readingTime, preview },
   };
 };
 
