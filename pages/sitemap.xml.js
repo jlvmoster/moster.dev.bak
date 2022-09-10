58c99b3cfd8a4a8e43098992af206e31b40a4e67
Jalo Moster <jlvmoster@gmail.com>
2022-09-10 02:06:53 -0400

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,36 +1,32 @@
-import { Component } from 'react';
 import { getAllBlogPostSlugs } from '@/lib/api';
 
 const WEBSITE_URL = 'https://moster.dev';
 
-const createSitemap = slugs => `<?xml version="1.0" encoding="UTF-8"?>
+const generateSitemap = slugs => `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <url>
   <loc>${WEBSITE_URL}</loc>
-  <changefreq>daily</changefreq>
 </url>
 <url>
   <loc>${WEBSITE_URL}/blog</loc>
-  <changefreq>daily</changefreq>
 </url>
 <url>
   <loc>${WEBSITE_URL}/archive</loc>
-  <changefreq>daily</changefreq>
 </url>
-${slugs.map(slug => `<url><loc>${`${WEBSITE_URL}/blog/${slug}`}</loc><changefreq>daily</changefreq></url>`).join('\n')}
+${slugs.map(slug => `<url><loc>${`${WEBSITE_URL}/blog/${slug}`}</loc></url>`).join('\n')}
 </urlset>
 `;
 
-class Sitemap extends Component {
-  static async getInitialProps({ res }) {
-    const posts = (await getAllBlogPostSlugs()) ?? [];
-    const slugs = posts.map(post => post.fields.slug);
-    const sitemap = createSitemap(slugs);
+const Sitemap = () => {};
 
-    res.setHeader('Content-Type', 'text/xml');
-    res.write(sitemap);
-    res.end();
-  }
-}
+export const getServerSideProps = ({ res }) => {
+  const posts = (await getAllBlogPostSlugs()) ?? [];
+  const slugs = posts.map(post => post.fields.slug);
+  const sitemap = generateSitemap(slugs);
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+};
 
 export default Sitemap;
