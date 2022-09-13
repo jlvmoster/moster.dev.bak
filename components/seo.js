9f8f5d28c5221de5576da2a8778901462ff71b61
Jalo Moster <jlvmoster@gmail.com>
2022-09-13 10:44:49 -0400

    Enhance SEO meta tags

--- components/seo.js
+++ components/seo.js
@@ -14,14 +14,19 @@ const SEO = ({ title, description, pathname, children }) => {
   return (
     <Head>
       <title>{seo.title}</title>
-      <meta name='description' content={seo.description} />
-      <meta name='image' content={seo.image} />
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:title' content={seo.title} />
       <meta name='twitter:description' content={seo.description} />
       <meta name='twitter:image' content={seo.image} />
       <meta name='twitter:url' content={seo.url} />
       <meta name='twitter:creator' content={seo.twitterUsername} />
+      <meta name='title' property='og:title' content={seo.title} />
+      <meta name='description' property='og:description' content={seo.description} />
+      <meta name='image' property='og:image' content={seo.image} />
+      <meta name='image:alt' property='og:image:alt' content={seo.description} />
+      <meta name='url' property='og:url' content={seo.url} />
+      <meta name='site_name' property='og:site_name' content={defaultTitle} />
+      <meta name='hostname' content='moster.dev' />
       {children}
     </Head>
   );
